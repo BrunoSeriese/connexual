b84652c1881828acb5c6dca278ac1711a2784961
pretraket <43110626+pretraket@users.noreply.github.com>
2019-10-24 21:31:02 +0200

    added section for pricing

--- js/script.js
+++ js/script.js
@@ -45,3 +45,47 @@ $(".container__overlay--2").hover(function(){
 $(".message--1").toggleClass("lowpacity");
 
 });
+
+
+$(".paid").click(function(){
+  console.log($("#paid").css("background-color"));
+  if($("#paid").css("background-color") =="rgb(76, 58, 56)")
+  {
+    $(".pricing__right").toggleClass("active__item");
+      $(".pricing__left").toggleClass("active__item");
+  }
+
+});
+
+
+$(".free").click(function(){
+    console.log($("#free").css("background-color"));
+    if($("#free").css("background-color") =="rgb(76, 58, 56)"){
+  $(".pricing__left").toggleClass("active__item");
+    $(".pricing__right").toggleClass("active__item");
+  }
+});
+
+
+
+$(".free").click(function(){
+  console.log($("#first").css("opacity"));
+if($("#first").css("opacity") == 0){
+  console.log("succes!");
+  $(".pricing__feature--1").toggleClass("lowpacity--2");
+  $(".pricing__feature--2").toggleClass("lowpacity--2");
+}
+
+
+});
+
+$(".paid").click(function(){
+  console.log($("#second").css("opacity"));
+if($("#second").css("opacity") == 0){
+  console.log("succes!");
+  $("#first").toggleClass("lowpacity--2");
+  $("#second").toggleClass("lowpacity--2");
+}
+
+
+});
